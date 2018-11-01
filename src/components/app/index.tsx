e391b5bf5ff086cd3659fd521f967fe284d3a5e5
Saveli Tomak <ollcaier@gmail.com>
2018-11-01 20:25:54 +0300

    create grid for App component

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
 import { IMapStateToAppProps } from "./container";
+import styles from "./styles.scss";
 
 type TAppProps = IMapStateToAppProps;
 
 export class App extends React.Component<TAppProps> {
   public render() {
     return (
-      <div/>
+      <div className={styles["app-container"]}>
+        <div className={styles["search-container"]}/>
+        <main className={styles["content-container"]}>
+          <section className={styles["artist-container"]}/>
+          <section className={styles["events-container"]}/>
+        </main>
+      </div>
     );
   }
 }
---
+++ src/components/app/styles.scss.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  "app-container": string;
+  "artist-container": string;
+  "content-container": string;
+  "events-container": string;
+  "search-container": string;
+};
+
+export = styles;
